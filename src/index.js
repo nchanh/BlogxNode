02e831f756b1bad2611499ff90488051438afd3c
Nguyen Chanh <nguyenchanh0306@gmail.com>
2022-06-25 16:15:29 +0700

    feat: Create public folder, add scss

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import express from 'express';
 import morgan from 'morgan';
 import { create } from 'express-handlebars';
 
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
+
+// Call to public folder
+app.use(express.static(__dirname + '/public'));
 
 // HTTP Logger
 app.use(morgan('combined'));
@@ -14,7 +21,7 @@ const handlebars = create({
 });
 app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
-app.set('views', './src/resource/views/');
+app.set('views', __dirname + '/resource/views/');
 
 app.get('/', (req, res) => {
   res.render('home');
