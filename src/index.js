91142f9a3e6b617f7b41dc9e6ebd72c68e2e283d
Nguyen Chanh <nguyenchanh0306@gmail.com>
2022-06-25 15:03:43 +0700

    feat: Add Template Engine

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import morgan from 'morgan';
+import { create } from 'express-handlebars';
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'))
+
+const handlebars = create({
+  extname: '.hbs'
+});
+
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
+app.set('views', './src/resource/views/');
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost: ${port}`)
+})
